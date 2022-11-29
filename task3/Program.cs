9857d388c91dee033e181ac3231c6b0188e41aa9
ShamaKung <shamaturin@gmail.com>
2022-11-30 02:25:58 +0500

    task3

---
+++ task3/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+if(a % 2 == 1)
+{
+    Console.WriteLine($"Число {a} нечетное");
+}
+else 
+{
+    Console.WriteLine($"Число {a} четное");
+}
