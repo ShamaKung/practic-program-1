25b995c61967d7a725cb8dc4127c80bd38441f12
ShamaKung <shamaturin@gmail.com>
2022-11-30 01:00:31 +0500

    решение задачи

---
+++ task1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите 1-ое число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите 2-ое число");
+int b = Convert.ToInt32(Console.ReadLine());
+if(a < b)
+    {
+        Console.Write("Большее число ");
+        Console.WriteLine(b);
+    }
+else 
+    {
+        Console.Write("Большее число ");
+        Console.WriteLine(a);
+    }
